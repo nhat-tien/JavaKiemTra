a6bb6443de4754f64a63259f15565de764da31ff
Khong Biet Code - <khongbietcode1@gmail.com>
2024-12-03 10:37:08 +0700

    toi da lam xong

---
+++ JavaKT/src/main/java/com/mycompany/javakt/JavaKT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javakt;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author To Trung Tin
+ */
+public class JavaKT {
+
+    public static void main(String[] args) {
+        PHONGBAN jv = new PHONGBAN();
+        jv.setVisible(true);
+        jv.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
