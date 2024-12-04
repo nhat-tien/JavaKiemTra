be35f139a00493e31a9c4906baa0f36d89986c57
Khong Biet Code - <khongbietcode1@gmail.com>
2024-12-04 10:29:16 +0700

    lan thu 2 cua toi

--- JavaKT/src/main/java/com/mycompany/javakt/MysqlConnection.java
+++ JavaKT/src/main/java/com/mycompany/javakt/MysqlConnection.java
@@ -4,10 +4,30 @@
  */
 package com.mycompany.javakt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author To Trung Tin
  */
 public class MysqlConnection {
+    private final static String username = "root";
+    private final static String password = "";
+    private final static String connectionURL = "jdbc:mysql://localhost/javadb";
     
+    public static Connection createConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(connectionURL, username, password);
+            System.out.println("Kết nối thành công");
+        } catch (SQLException e) {
+           System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+  
 }
